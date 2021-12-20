cbd02eced4b470cf5789e005630604b8cc4f0fef
Edgaras <edgaras318@gmail.com>
2021-12-21 00:39:51 +0100

    initial packages and clean up

--- src/main.js
+++ src/main.js
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Bootstrap and bootstrap icons
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 createApp(App).use(router).mount('#app')
