aa0bc3569c53597cc4e996f3a063f77476439bb2
Edgaras <edgaras318@gmail.com>
2021-12-21 14:46:52 +0100

    feat: routes guard

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Home from '../views/Home.vue'
 import WizzkidDetails from '@/views/WizzkidDetails.vue'
 import Register from '@/views/auth/Register.vue'
 import Login from '@/views/auth/Login.vue'
+import getUser from "@/composables/getUser";
+
 const routes = [
   {
     path: '/',
@@ -31,5 +33,30 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+const { user } = getUser();
+router.beforeEach((to, from, next) => {
+  if (to.name == "WizzkidDetails") {
+    if (localStorage.token && user.value) {
+      next();
+    }
+    else {
+      next('Login');
+    }
+  }
+  else if (to.name == "Login") {
+
+    if (!localStorage.token && !user.value) {
+      next();
+    }
+    else {
+      next('/');
+    }
+  }
+  else {
+    next();
+  }
+})
+
 
 export default router
